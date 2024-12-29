778b82885005c5a818c1246893402c5dd706d5e2
yyxcv <yxcv@github.com>
2024-12-29 21:51:33 +0100

    implement "unpaved"

--- src/js/modules/layers/radlkarte.js
+++ src/js/modules/layers/radlkarte.js
@@ -1,77 +1,107 @@
-const widthFunctionPriority0 = ["interpolate", ["linear"], ["zoom"], 9, 1, 20, 10];
+const widthFunctionPriority0 = [
+  "interpolate", ["linear"], ["zoom"],
+  9, 1,
+  20, 10
+];
+
+const lineColors = [
+  "match", ["get", "stress"],
+  "0", "#004B67",
+  "1", "#51A4B6",
+  "2", "#FF6600",
+  "#000"
+];
+
+
+const lineDashArray = [2.5, 3];
+
+//steil
+//einbahn
+
+const getUnpavedLayersFor = function (sourceName) {
+  return [
+    {
+      "id": "priority0" + sourceName + "_unpaved",
+      "type": "line",
+      "source": sourceName,
+      "filter": ["all", ["==", "priority", "0"], ["==", "unpaved", "yes"]],
+      "paint": {
+        "line-color": lineColors,
+        "line-dasharray": lineDashArray,
+        "line-width": widthFunctionPriority0
+      }
+    },
+    {
+      "id": "priority1" + sourceName + "_unpaved",
+      "type": "line",
+      "source": sourceName,
+      "filter": ["all", ["==", "priority", "1"], ["==", "unpaved", "yes"]],
+      "paint": {
+        "line-color": lineColors,
+        "line-dasharray": lineDashArray,
+        "line-width": 1
+      },
+      "minzoom": 12,
+    },
+    {
+      "id": "priority2" + sourceName + "_unpaved",
+      "type": "line",
+      "source": sourceName,
+      "filter": ["all", ["==", "priority", "2"], ["==", "unpaved", "yes"]],
+      "paint": {
+        "line-color": lineColors,
+        "line-dasharray": lineDashArray,
+        "line-width": 1
+      },
+      "minzoom": 14,
+    },
+  ];
+}
+
+const getPavedRadlkarteLayersFor = function (sourceName) {
+  return [
+    {
+      "id": "priority0" + sourceName,
+      "type": "line",
+      "source": sourceName,
+      "filter": ["all", ["==", "priority", "0"], ["!=", "unpaved", "yes"]],
+      "paint": {
+        "line-color": lineColors,
+        "line-width": widthFunctionPriority0
+      }
+    },
+    {
+      "id": "priority1" + sourceName,
+      "type": "line",
+      "source": sourceName,
+      "filter": ["all", ["==", "priority", "1"],["!=", "unpaved", "yes"]],
+      "paint": {
+        "line-color": lineColors,
+        "line-width": 1
+      },
+      "minzoom": 12,
+    },
+    {
+      "id": "priority2" + sourceName,
+      "type": "line",
+      "source": sourceName,
+      "filter": ["all", ["==", "priority", "2"],["!=", "unpaved", "yes"]],
+      "paint": {
+        "line-color": lineColors,
+        "line-width": 1
+      },
+      "minzoom": 14,
+    },
+  ];
+}
+
 
 const getRadlkarteLayersFor = function (sourceName) {
   return [
-    {
-      "id": "stress0_priority0" + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "0"], ["==", "priority", "0"]],
-      "paint": { "line-color": "#004B67", "line-width": widthFunctionPriority0 }
-    },
-    {
-      "id": "stress0_priority1" + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "0"], ["==", "priority", "1"]],
-      "paint": { "line-color": "#004B67", "line-width": 1 },
-      "minzoom": 12,
-    },
-    {
-      "id": "stress0_priority2" + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "0"], ["==", "priority", "2"]],
-      "paint": { "line-color": "#004B67", "line-width": 1 },
-      "minzoom": 14,
-    },
-    {
-      "id": "stress1_priority0" + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "1"], ["==", "priority", "0"]],
-      "paint": { "line-color": "#51A4B6", "line-width": widthFunctionPriority0 }
-    },
-    {
-      "id": "stress1_priority1" + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "1"], ["==", "priority", "1"]],
-      "paint": { "line-color": "#51A4B6", "line-width": 1 },
-      "minzoom": 12,
-    },
-    {
-      "id": "stress1_priority2" + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "1"], ["==", "priority", "2"]],
-      "paint": { "line-color": "#51A4B6", "line-width": 1 },
-      "minzoom": 14,
-    },
-    {
-      "id": "stress2_priority0" + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "2"], ["==", "priority", "0"]],
-      "paint": { "line-color": "#FF6600", "line-width": widthFunctionPriority0 }
-    },
-    {
-      "id": "stress2_priority1" + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "2"], ["==", "priority", "1"]],
-      "paint": { "line-color": "#FF6600", "line-width": 1 },
-      "minzoom": 12,
-    },
-    {
-      "id": "stress2_priority2"  + sourceName,
-      "type": "line",
-      "source": sourceName,
-      "filter": ["all", ["==", "stress", "2"], ["==", "priority", "2"]],
-      "paint": { "line-color": "#FF6600", "line-width": 1 },
-      "minzoom": 14,
-    }
-  ];
+    ...getPavedRadlkarteLayersFor(sourceName),
+    ...getUnpavedLayersFor(sourceName)
+  ]
 }
+
 
 export default getRadlkarteLayersFor;
